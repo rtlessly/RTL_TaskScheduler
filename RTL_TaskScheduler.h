01d2dde6bfd5a37c7d175bdb2691341e5f26cd6a
rtlessly <tlessly@hotmail.com>
2018-12-27 09:38:25 -0600

    Corrected bugs. Added documentation.

--- RTL_TaskScheduler.h
+++ RTL_TaskScheduler.h
@@ -1,8 +1,12 @@
 /*******************************************************************************
-Header file for the WorkTask class.
+Header file for the TaskScheduler class.
 *******************************************************************************/
 #ifndef _RTL_TaskScheduler_h_
 #define _RTL_TaskScheduler_h_
+
+
+// Typedef of a task function
+typedef bool (*TaskFunc)(void);     /* Size = 2 bytes*/
 
 
 //******************************************************************************
@@ -38,24 +42,29 @@ Header file for the WorkTask class.
 /// precious RAM memory by allocating a queue that is larger than what is actually
 /// used, while new carries a lot of overhead and also is less memory efficient).
 //******************************************************************************
-
-
-// Typedef of a task function
-typedef bool (*TaskFunc)(void);     /* Size = 2 bytes*/
-
-
 class ATask                         /* Size = 4 bytes*/
 {
     friend class  TaskScheduler;
 
+	//**************************************************************************
+	/// The function that the task executes. Derived classes must implement this 
+	/// function.
+	//**************************************************************************
     public: virtual bool Execute() = 0;
 
+	//**************************************************************************
+	/// Inserts a new task after this task in the linked list.
+	//**************************************************************************
     public: void Link(ATask& task)
     {
-        task._pNextTask == _pNextTask;
+        task._pNextTask = _pNextTask;
         _pNextTask = &task;
     };
 
+	//**************************************************************************
+	/// Removes a task from the linked list of tasks if it is the next task linked
+	/// to this task.
+	//**************************************************************************
     protected: void Unlink(ATask& task)
     {
         if (_pNextTask == &task)
@@ -65,62 +74,112 @@ class ATask                         /* Size = 4 bytes*/
         }
     }
 
+	/// The pointer to the next task in the linked list. 
     private: ATask* _pNextTask;
 };
 
 
+//******************************************************************************
+/// An implmentation of ATask that executes the function passed to the constuctor
+/// as the task function.
+//******************************************************************************
 class Task : public ATask           /* Size = sizeof(ATASK) + 2 = 4 + 2 = 6 bytes*/
 {
+	//**************************************************************************
+	/// Constructs an instance of the Task class with the function to execute.
+	//**************************************************************************
     public: Task(TaskFunc func) : _function(func) { };
 
+	//**************************************************************************
+	/// Executes the function bound to this instance in the constructor.
+	//**************************************************************************
     public: bool Execute() { return _function(); };
 
+	/// The function to execute for the task.
     private: TaskFunc _function;
 };
 
 
+//******************************************************************************
+/// Schedules the execution of tasks and manages the linked list of tasks.
+//******************************************************************************
 class TaskScheduler                 /* Size = 4 bytes*/
 {
     DECLARE_CLASSNAME;
 
+	//**************************************************************************
+	/// Constructs an instance of TaskScheduler.
+	//**************************************************************************
     public: TaskScheduler() : _pFirstTask(nullptr) { };
 
-    /***************************************************************************
-    Public methods
-    ***************************************************************************/
+	//**************************************************************************
+	/// Dispatches the next task.
+	//**************************************************************************
     public: void Dispatch();
 
+	//**************************************************************************
+	/// Adds a task to the scheduler.
+	//**************************************************************************
     public: void Add(ATask& task);
 
+	//**************************************************************************
+	/// Adds a task at a specific priority (index into the scheduler queue).
+	//**************************************************************************
     public: void InsertAt(ATask& task, uint16_t priority);
 
+	//**************************************************************************
+	/// Adds a task before another task.
+	//**************************************************************************
     public: void InsertBefore(ATask& task, ATask& beforeTask);
 
+	//**************************************************************************
+	/// Adds a task after another task.
+	//**************************************************************************
     public: void InsertAfter(ATask& task, ATask& afterTask);
 
+	//**************************************************************************
+	/// Removes a task from the scheduler.
+	//**************************************************************************
     public: void Remove(ATask& task);
 
+	//**************************************************************************
+	/// Diagnostic method to display the current queue of tasks.
+	//**************************************************************************
     public: void DumpQueue(const __FlashStringHelper* message=nullptr);
 
-    /***************************************************************************
-    Internal implementation
-    ***************************************************************************/
+	
+    //**************************************************************************
+	/// Returns the last task in scheduler queue.
+	//**************************************************************************
     private: ATask* LastTask();
 
+	//**************************************************************************
+	/// Finds a particular task in the scheduler queue.
+	//**************************************************************************
     private: ATask* Find(Task* pTask);
 
+	//**************************************************************************
+	/// Returns the task previous to the specified task.
+	//**************************************************************************
     private: ATask* FindPrev(Task* pTask);
 
+	//**************************************************************************
+	/// Links a task into the queue after the specified task.
+	//**************************************************************************
     private: void LinkTask(ATask* pTask, ATask* pNewTask);
 
+	//**************************************************************************
+	/// Links a task  as the first task in the queue.
+	//**************************************************************************
     private: void LinkFirstTask(ATask* pNewTask);
 
+	//**************************************************************************
+	/// Links a task as the last task in the queue.
+	//**************************************************************************
     private: void LinkLastTask(ATask* pNewTask);
 
 
-    /***************************************************************************
-    Internal state
-    ***************************************************************************/
+	/// The first task in the linked lists of task (i.e., the head of the list)
     private: ATask* _pFirstTask;
 };
 
@@ -133,6 +192,9 @@ class TaskScheduler                 /* Size = 4 bytes*/
 //}
 
 
+//******************************************************************************
+/// Links a task  as the first task in the queue.
+//******************************************************************************
 inline void TaskScheduler::LinkFirstTask(ATask* pNewTask)
 {
     pNewTask->_pNextTask = _pFirstTask;
